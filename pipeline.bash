e09a118497b99b69d90d6fe80d7f6221a7987b01
lulunots <105442686+lulunots@users.noreply.github.com>
2023-06-08 22:19:07 +0200

    Update pipeline.bash

--- pipeline.bash
+++ pipeline.bash
@@ -1,15 +1,32 @@
-# execute in the virpool environment
-
+# execute in the virpool environment with minimap2 installed additionally
 
 # ALIGNMENT
 # input:  /tudelft.net/staff-umbrella/SARSCoV2WastewaterBratislava/lulu/read_filtering/second/
 # output: /tudelft.net/staff-umbrella/SARSCoV2WastewaterBratislava/lulu/alignment/after_readfilter/
 
 # output of the alignment
-loc1="/tudelft.net/staff-umbrella/SARSCoV2WastewaterBratislava/lulu/alignment/after_readfilter"
+out_path="/tudelft.net/staff-umbrella/SARSCoV2WastewaterBratislava/lulu/bootstrapping/abundances"
 
 # input of the reference genome .fasta file
-loc4="/tudelft.net/staff-umbrella/SARSCoV2WastewaterBratislava/lulu/alignment/EPI_ISL_402124.fasta"
+reference="/tudelft.net/staff-umbrella/SARSCoV2WastewaterBratislava/lulu/alignment/EPI_ISL_402124.fasta"
+
+# input of the fastq files
+in_path="/tudelft.net/staff-umbrella/SARSCoV2WastewaterBratislava/lulu/bootstrapping/resampled_datasets"
+
+# number of resamples as defined in the previous step
+num=100
+
+for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+do
+  for ((j=1;j<=num;j++))
+  do
+    $out_path/"${num}"/sample_"${i}"/boot_sample"${i}"_"${j}".fastq
+    minimap2 -ax map-ont $reference $in_path/"${num}"/sample_"${i}"/boot_sample"${i}"_"${j}".fastq > $out_path/alignment_boot_sample"${i}"_"${j}".sam
+    samtools sort -o $out_path/alignment_boot_sample"${i}"_"${j}".bam $out_path/alignment_boot_sample"${i}"_"${j}".sam
+    samtools index $out_path/alignment_boot_sample"${i}"_"${j}".bam
+  done
+  echo "done with sample "${i}""
+done
 
 for file in /tudelft.net/staff-umbrella/SARSCoV2WastewaterBratislava/lulu/read_filtering/second/*
 do
